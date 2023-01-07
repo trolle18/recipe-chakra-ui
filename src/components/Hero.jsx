ca08d233d18101ce185da7419b5e87bad94a5f69
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-07 16:22:59 +0100

    removed unused code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,6 @@ export default function Hero() {
         w={'full'}
         h={{ base: '60vh', md: '50vh', lg: '40vh'}}
         maxH={{ base: '400px', md: '500px', lg: '600px'}}
-        // maxH={'500px'}    
         backgroundImage={
           'url(https://images.unsplash.com/photo-1653233797663-376044b096d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)'
         }
@@ -17,14 +16,11 @@ export default function Hero() {
         backgroundPosition={'center center'}>
 
         <Flex
-          // border={'solid red 1px'}
           h={'full'}
           w={'full'}
-          // px={useBreakpointValue({ base: 4, md: 8 })}
           bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
           >
             <Flex
-            // border={'solid red 1px'}
             m={'auto'}
             justify={'center'}
             alignItems={'center'}
